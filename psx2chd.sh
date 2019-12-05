126f85d65414ba6cfcbd9c43578ca37bb53c1ae4
Kashaiahyah85 <kashaiahyah85@icloud.com>
2019-12-04 16:16:24 -0800

    Still progress box

--- psx2chd.sh
+++ psx2chd.sh
@@ -137,7 +137,7 @@ function compressRoms() {
         FILE_OUT="${FILE_IN%.*}.chd"
          cd "$ROMS_DIR" || exit
          echo chdman createcd -i \""$FILE_IN"\" -o \""$FILE_OUT"\" > "$CHD_SCRIPT"
-	 dialogProgress $(sh "$CHD_SCRIPT")
+	 dialogProgress "$(sh $CHD_SCRIPT)"
 		 sed 's/.'[0-9]'% complete... (ratio='[0-9][0-9]'.'[0-9]'%)//'
 	 #| dialog --progressbox "Compressing ${FILE_IN%.*}" 10 80
 		#| grep \\% \# | sed 's/Compressing, //' \
